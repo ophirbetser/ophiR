b1b95a313e37e7d2bdca5819440515bda6d1e45b
Ophir Betser <ophirbetser@Ophirs-MacBook-Air.local>
2021-09-16 04:08:31 +0300

    add N_i

--- R/hello.R
+++ R/hello.R
@@ -57,9 +57,15 @@ u <- function(v) {
 un <- function(v) {
   data.table::uniqueN(v)
 }
-unn <- function(v) {
-  data.table::uniqueN(v)
+
+N_i <- function(dt, i) {
+  dt <- data.table::as.data.table(dt)
+  dt[, .N, by = c(names(dt)[i])]
 }
 
+N_name <- function(dt, name) {
+  dt <- data.table::as.data.table(dt)
+  dt[, .N, by = c(name)]
+}
 
 
